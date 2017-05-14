faab2f53e107d81bf970dc1ecad8b9e340eff2fd
Christian Walde <walde.christian@googlemail.com>
2017-05-14 10:57:01 +0200

    ensure cpanm runs in non-interactive mode

--- lib/Distribution/Smoke.pm
+++ lib/Distribution/Smoke.pm
@@ -231,7 +231,7 @@ sub _build_dist {
 
   $self->log("Building dist", $dist->{name}, "in dir", $ipath);
 
-  my $cmd = "cpanm -l $ipath --verbose $dist->{name} > $lpath 2>&1";
+  my $cmd = "cpanm -l $ipath --verbose $dist->{name} --no-interactive > $lpath 2>&1";
   $self->log_verbose("Running", $cmd);
 
   my $res = `$cmd`;
@@ -321,7 +321,7 @@ sub test_distributions {
       }
       else {
         # XXX - Write to temp file, move into place (save memory)
-        my $cmd = "cpanm -l $ipath --test-only --verbose $dist 2>&1";
+        my $cmd = "cpanm -l $ipath --test-only --verbose $dist --no-interactive 2>&1";
         $self->log_verbose("\tRunning", $cmd);
 
         $res = `$cmd`;
